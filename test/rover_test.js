c3f76a547fc61c0fc06b4b663d0ed2d912892f21
neshka <neshka_mind@yahoo.com>
2019-02-06 13:37:25 +0000

    add positions test

--- test/rover_test.js
+++ test/rover_test.js
@@ -5,15 +5,28 @@ const expect = chai.expect
 const rover = require("../app/rover");
 
 describe('Rover', function() {
+
   it('should exist', function() {
       var rover = require("../app/rover");
       expect(rover).to.not.be.undefined;
   });
 
-  it('should create rover object', function() {
+  it('object rover should have direction', function() {
     var rover1 = new rover.Rover(0,0,'N');
     expect(rover1.direction).equal('N');
   });
+
+  it('object rover should have positionX', function() {
+    var rover1 = new rover.Rover(0,0,'N');
+    expect(rover1.positionX).equal(0);
+  });
+
+  it('object rover should have positionY', function() {
+    var rover1 = new rover.Rover(0,0,'N');
+    expect(rover1.positionY).equal(0);
+  });
+
+
 
 
 });
